a6b20fc7eb181aa055efaea4123156ec8ed2655e
maya-abdoh <92648274+maya-abdoh@users.noreply.github.com>
2021-10-16 22:21:19 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,32 @@
+
+Console.log("welcom") </script> <script>
+<script src ="file name"></script>
+Let age =19;
+Const name ="maya";
+Console.log(age); //19
+console.log(typeof age);
+object:
+let cat{
+name:"snow white";
+color:white;
+age:1;
+}
+Console.log (cat.name); // snow white
+Console.log (cat.age); //  1
+Cat.name="poto";
+Console.log (cat.name); // poto
+Console.log (cat.age); //  1
+Cat ['color']="black";
+Console.log(cat['color']); // black
+List:
+Let names =["ahmad","maya","mohamad","yara"];
+names[4]="aya"; 
+console.log(names); // ahmad/maya/mohamad/yara/aya
+function sum (y , z) {
+return (y+z);
+}
+
+function y(name){
+console.log("hello" +name);
+}
+Y( "maya"); // hello maya
